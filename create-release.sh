12e56e8a125421027445d483383fe98d8c87f017
mr_nholi <166160218+nholi@users.noreply.github.com>
2024-08-02 19:00:08 +0200

    Workflow fix 6
    I have skill issues pt.2

--- create-release.sh
+++ create-release.sh
@@ -7,5 +7,3 @@ dotnet publish --configuration Release ./HDiffPatchCreator --output "./Release-L
 cp "HDiffPatchCreator/bin/Release/net8.0/7zz" "Release-Linux-x64/Patch Creator/"
 cp "HDiffPatchCreator/bin/Release/net8.0/hdiffz" "Release-Linux-x64/Patch Creator/"
 cp "HDiffPatchCreator/bin/Release/net8.0/hpatchz" "Release-Linux-x64/Patch Creator/"
-
-pause
