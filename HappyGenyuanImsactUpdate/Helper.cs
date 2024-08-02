a9be46e5deee16b75114491c2b7a349698e45096
mr_nholi <166160218+nholi@users.noreply.github.com>
2024-08-02 18:28:43 +0200

    Fix Helper

--- HappyGenyuanImsactUpdate/Helper.cs
+++ HappyGenyuanImsactUpdate/Helper.cs
@@ -64,11 +64,11 @@ namespace HappyGenyuanImsactUpdate
             "\u0079\u0075\u0061\u006e\u0073\u0068\u0065\u006e",
             "\u0059\u0075\u0061\u006e\u0053\u0068\u0065\u006e",
             // Houtai: March 7th
-            "\u0053\u0074\u0061\u0072\u0052\u0061\u0069\u006c"
+            "\u0053\u0074\u0061\u0072\u0052\u0061\u0069\u006c",
             // "Reserved Executable Name:/\\<>" // OS exename == CN
             // Sleep
             "\u004a\u0075\u0065\u0051\u0075\u004c\u0069\u006e\u0067", // Former used
-            "\u005a\u0065\u006e\u006c\u0065\u0073\u0073\u005a\u006f\u006e\u0065\u005a\u0065\u0072\u006f",
+            "\u005a\u0065\u006e\u006c\u0065\u0073\u0073\u005a\u006f\u006e\u0065\u005a\u0065\u0072\u006f"
         };
 
         public static bool AnyCertainGameExists(string checkdir)
